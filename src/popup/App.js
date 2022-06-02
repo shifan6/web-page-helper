a587cd317fca8f92f0172ff4f2e5b337ad7d74f2
Fannie Shi <qys9309@qq.com>
2022-06-02 17:54:44 +0800

    V2.1：优化导入配置

--- src/popup/App.js
+++ src/popup/App.js
@@ -76,7 +76,7 @@ function App({ configs, location }) {
       const index = newConfigs.findIndex(it => it.id === config.id)
       newConfigs.splice(index, 1, config)
     } else {
-      config.id = new Date().getTime()
+      config.id = getUniqueId()
       newConfigs.push(config)
     }
     setView('config-list')
@@ -87,7 +87,7 @@ function App({ configs, location }) {
   
   const updateContentByConfig = function (config) {
     const { auto, type } = config
-    const name = `${ type }-${ auto ? 'add' : 'remove'}`
+    const name = `${ type }-${ auto ? 'add' : 'remove' }`
     switch (name) {
       case 'style-add':
         sendMessageToContent('addStyle', config)
@@ -125,11 +125,51 @@ function App({ configs, location }) {
     }
     const reader = new FileReader()
     reader.onload = function fileReadCompleted() {
-      const json = JSON.parse(reader.result)
-      const info = {}
-      pageConfigs.forEach(it => info['key-' + it.id] = it)
-      json.pageConfigs.forEach(it => info['key-' + it.id] = it)
-      const newConfigs = Object.keys(info).map(key => info[key])
+      const json = JSON.parse(reader.result) || {}
+      json.pageConfigs = json.pageConfigs || []
+
+      // 过滤原有的不合法数据
+      const origin = pageConfigs.reduce((result, current) => {
+        const { id, page, type, content } = current
+        if (id && page && type && content) {
+          result[id] = current
+        }
+        return result
+      }, {})
+
+      // 过滤导入的不合法数据
+      const add = json.pageConfigs.reduce((result, current) => {
+        const { id, page, type, content } = current
+        if (id && page && type && content) {
+          result[id] = current
+        }
+        return result
+      }, {})
+
+      // 新增配置 auto 为 false
+      const newConfigs = Object.keys(add).reduce((result, key) => {
+        const current = add[key]
+        const { id, page, type, content } = current
+        const originInfo = origin[id]
+        if (originInfo) {
+          // 处理相同id的配置：配置详情一致时过滤，不一致时新增（不对比name、auto）
+          if (page !== originInfo.page || type !== originInfo.type || content !== originInfo.content) {
+            result.push({
+              ...current,
+              auto: false,
+              id: getUniqueId()
+            })
+          }
+        } else {
+          result.push({
+            ...current,
+            auto: false
+          })
+        }
+        return result
+      }, [...pageConfigs])
+
+      // 更新配置、缓存（不用更新视图，因为新增的配置均是默认关闭）
       setPageConfigs(newConfigs)
       updatePageConfigStorage(newConfigs)
     }
@@ -152,6 +192,19 @@ function App({ configs, location }) {
         }
       })
     })
+  }
+
+  const getUniqueId = function () {
+    const len = 6
+    const chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
+    const maxPos = chars.length
+    const timestamp = new Date().getTime()
+    let result = ''
+    for (let i = 0; i < len; i++) {
+      result += chars.charAt(Math.floor(Math.random() * maxPos))
+    }
+    result += timestamp
+    return result
   }
 
   return (
